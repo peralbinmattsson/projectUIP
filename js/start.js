31f0ce76ea0461c257cec3f5273ecdd56efa23d8
peralbin <peralbin@ponty.se>
2015-02-10 11:11:23 +0100

    Fix start-page.

--- js/start.js
+++ js/start.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $('#upperLeft, #upperRight, #lowerLeft, #lowerRight').hover(function() {
+    $('#left, #right').hover(function() {
         $(this).css('border-color', '#26C6B0');
     }, function() {
         $(this).css('border-color', 'black');
