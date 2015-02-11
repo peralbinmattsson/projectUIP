7369490affa2e436f654a9f78641e55d37099b54
peralbin <peralbin@ponty.se>
2015-02-11 12:31:17 +0100

    Right list and search.

--- js/beer.js
+++ js/beer.js
@@ -1,17 +1,26 @@
 $(document).ready(function() {
-    var $beer = $('#leftList');
-    var $chosenBeer = $('#rightList');
+    var $leftList = $('#leftList');
+    var $rightList = $('#rightList');
 
-    function addBeer(beer) {
+    var leftListItem = 
+        "<li id='item' name='{{namn}}'><span>{{namn}}" +
+        "</span><span>{{pub_price}} kr</span>" +
+        "<button data-id='{{beer_id}}' name='{{namn}}' price='{{pub_price}}'" +
+        " id='add'>Add to order</button>" +
+        "</li>";
+
+    function listBeer(beer) {
         if (beer.namn != "") {
-            $beer.append("<li><span>" + 
-                beer.namn +
-                "</span><span>" +
-                beer.pub_price +
-                " kr" + "</span>" +
-                "<button id=\"add\">Add to order</button>" +
-                "</li>");
+            $leftList.append(Mustache.render(leftListItem, beer));
         }
+    }
+
+    function chooseBeer(name, id) {
+        $rightList.append("<li data-id=" + id + ">" + name + "</li>");
+    }
+
+    function addCost(price) {
+        $('#cost').html("<p cost=" + price + ">Total: " + price + "</p>");
     }
 
     $.ajax({
@@ -20,11 +29,51 @@ $(document).ready(function() {
         success: function(object) {
             data = object['payload'];
             $.each(data, function(i, beer) {
-                addBeer(beer);
+                listBeer(beer);
             });
         }
     });
-    $beer.delegate('#add', 'click', function() {
-        $chosenBeer.append("<li>dask</li>");
+
+    var price = parseInt("0");
+    var beerList = [];
+    var index = parseInt("0");;
+    //TODO: Store the number of times a specific beer has been chosen
+    $leftList.delegate('#add', 'click', function() {
+        id = $(this).attr('data-id');
+        index = index + parseInt("1");
+        name = $(this).attr('name');
+        thisPrice = $(this).attr('price');
+        if (beerList.indexOf(id) == parseInt("-1")) {
+            chooseBeer(name, id);
+            beerList[index] = id;
+        }
+        price = price + parseInt(thisPrice);
+        addCost(price);
+    });
+
+    $('.search').focus(function() {
+    });
+
+    $('.search').keypress(function(e) {
+        if (e.which == parseInt("13")) {
+            value = $(this).val();
+            $('li').each(function() {
+                name = $(this).attr('name');
+                if (value != name) {
+                    $(this).hide();
+                }
+            });
+        }
+        //if (e.which == parseInt("13")) {
+        //    $.ajax({
+        //        type: 'GET',
+        //        url: 'http://pub.jamaica-inn.net/fpdb/api.php?username=jorass&password=jorass&action=inventory_get',
+        //        success: function(object) {
+        //            data = object['payload'];
+        //            $.each(data, function(i, beer) {
+        //            });
+        //        }
+        //    });
+        //}
     });
 });
