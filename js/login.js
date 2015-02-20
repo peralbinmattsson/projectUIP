ced2697f430f68cae6b92acd9251bc3fd222ad10
Simon (^-^) <sivo4411@student.uu.se>
2015-02-20 13:27:04 +0100

    Fixed login

--- js/login.js
+++ js/login.js
@@ -1,24 +1,25 @@
 $(document).ready(function(){
 
   $("#login-btn").click(function(){
-  var username = $("#username-input").val();
-  var password = $("#password-input").val();
-  // Checking for blank fields.
-  if( username =='' || password ==''){
-    alert("Please fill all fields...!!!!!!");
-  }else {
-    authentification(username, password, 'index2.html');
-  }
-});
-
-function authentification(login, pass, ref) {
-  $.post('http://pub.jamaica-inn.net/fpdb/api.php?username=' + login + '&password='password + '&action=iou_get', function(data) {
-    if(data.type != "error"){
-      //TO DO: get id
-        document.location.href=ref; 
-    }
-    else {
-      alert('Error: ' + data.payload.msg);
-    }
+      var username = $("#username-input").val();
+      var password = $("#password-input").val();
+      // Checking for blank fields.
+      if( username =='' || password ==''){
+        alert("Please fill all fields...!!!!!!");
+      }else {
+        authentification(username, password, 'index2.html');
+      };
   });
-};
+
+  function authentification(login, pass, ref) {
+    $.post('http://pub.jamaica-inn.net/fpdb/api.php?username=' + login + '&password=' + pass + '&action=iou_get', function(data) {
+      if(data.type != "error"){
+        //TO DO: get id
+          document.location.href=ref; 
+      }
+      else {
+        alert('Error: ' + data.payload[0].msg);
+      };
+    });
+  };
+});
