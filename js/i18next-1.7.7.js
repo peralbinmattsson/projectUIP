2f2a8873beedec593a8aebecf6eac40c78d056a2
thevanthony <thev.anthony@gmail.com>
2015-02-25 12:09:39 +0100

    intertionalization handler

--- js/i18next-1.7.7.js
+++ js/i18next-1.7.7.js
@@ -19,7 +19,7 @@
             var n = 0;
             if (arguments.length > 0) {
                 n = Number(arguments[1]);
-                if (n != n) { // shortcut for verifying if it's NaN
+                if (n != n) {// shortcut for verifying if it's NaN
                     n = 0;
                 } else if (n != 0 && n != Infinity && n != -Infinity) {
                     n = (n > 0 || -1) * Math.floor(Math.abs(n));
