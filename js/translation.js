fd6e42162727d9567a85dc179b9f8115c1c863db
thevanthony <thev.anthony@gmail.com>
2015-02-25 12:51:38 +0100

    Update translation.js

--- js/translation.js
+++ js/translation.js
@@ -1,11 +1,3 @@
 $(document).ready(function(){
-  language_complete = navigator.language.split("-");
-  language = (language_complete[0]);
-  console.log("Sprache (root): %s", language);
-
-  i18n.init({ lng: language, debug: true }, function() {
-      // save to use translation function as resources are fetched
-      $(".menu").i18n();
-      $("headline").i18n();
-  });
+      $("#menu").i18n();
 });
