b2087a9bf51f4b1200283d0e87e2b81d26204a9e
Simon (^-^) <sivo4411@student.uu.se>
2015-02-26 14:41:31 +0100

    Login stuff

--- js/login.js
+++ js/login.js
@@ -1,4 +1,30 @@
+function print (arg) {console.log(arg);}
+
+function authentification(login, pass, ref) {
+  var admins = ["ervtod", "hirchr", "jorass", "saskru", "svetor"];
+  $.post('http://pub.jamaica-inn.net/fpdb/api.php?username=' + login + '&password=' + pass + '&action=iou_get', function(data) {
+    if(data.type != "error"){
+      //TO DO: get id
+        localStorage.setItem("user", login);
+        localStorage.setItem("user_id", data.payload[0].user_id);
+        localStorage.setItem("assets", data.payload[0].assets);
+        localStorage["isLoggedIn"] =  true;
+      
+        if ($.inArray(login, admins) > -1) {
+          localStorage.setItem("isAdmin", true);
+        } else{
+          localStorage.setItem("isAdmin", false);
+        };
+        window.location.href=ref; 
+    }
+    else {
+      alert('Error: ' + data.payload[0].msg);
+    };
+  });
+};
+
 $(document).ready(function(){
+  localStorage.setItem("isLoggedIn", false);
 
   $("#login-btn").click(function(){
       var username = $("#username-input").val();
@@ -11,15 +37,4 @@ $(document).ready(function(){
       };
   });
 
-  function authentification(login, pass, ref) {
-    $.post('http://pub.jamaica-inn.net/fpdb/api.php?username=' + login + '&password=' + pass + '&action=iou_get', function(data) {
-      if(data.type != "error"){
-        //TO DO: get id
-          document.location.href=ref; 
-      }
-      else {
-        alert('Error: ' + data.payload[0].msg);
-      };
-    });
-  };
 });
