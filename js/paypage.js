a4f055a8d5d1f321ff3a232010895e7022b8489c
Maryamshr <m.shirazinasab@yahoo.com>
2015-02-26 12:55:32 +0100

    last update

--- js/paypage.js
+++ js/paypage.js
@@ -1,48 +1,64 @@
 
-var rightListItem =
-    "<li data-id='{{id}}'>{{name}} <span id='{{id}}'>({{amount}})</span><p><br>" +
-    "{{price}} kr</p><button class='button' data-id='{{id}}'" +
-    "id=Cancel price='{{price}}'>X</button></li>";
 
 
 
 $(document).ready(function() {
-
+    var $rightList = $('#rightList');
+    var rightListItem =
+        "<li data-id='{{id}}'>{{name}} <span id='{{id}}'>({{amount}})</span><p><br>" +
+        "{{price}} kr</p><button class='button' data-id='{{id}}'" +
+        "id=Cancel price='{{price}}'>X</button></li>";
 
     orderList = JSON.parse( localStorage.getItem("order"));
     console.log(orderList);
 
     $.each(orderList, function(key, value) {
-        $('#rightList').append(Mustache.render(rightListItem, value));
+        $rightList.append(Mustache.render(rightListItem, value));
     });
 
 
-    total = localStorage.getItem("total");
+    var price = {
+        total: 0,
+        //Methods
+        addCost: function() {
+            var priceObject = this;
+            $cost.html(Mustache.render(costItem, priceObject));
+        }
+    };
+    $rightList.delegate('#remove', 'click', function(){
+        var id = $(this).attr('data-id');
+        var thisPrice = $(this).attr('price');
+        var thisOrder = order.orderList[id];
+        if (thisOrder != undefined){
+            removeBeer(id);
+            price.total = price.total-parseInt(thisPrice);
+            price.addCost(price);
+        };
+    });
+    function removeBeer(id) {
+        var thisOrder = this.orderList[id];
+        if (thisOrder['amount'] == 1) {
+            delete this.orderList[id];
+            $("ul li[data-id=" + id + "]").remove();
+        } else {
+            thisOrder['amount'] -= 1;
+            $('#' + thisOrder['id'] + '').text("(" + thisOrder['amount'] + ")");
+        }
+    }
+        total = localStorage.getItem("total");
 
 // The popup windows  after clicking on pay or cancel button
-    $('#PaymentButton').on('click', function() {
-        alert("Thanks for your shopping :)");
-    });
-    $('#CancelButton').on('click', function() {
-        var x;
-        if (confirm("Are You sure that you want to finish transaction?!") == true) {
-            x = window.open("index.html");
+        $('#PaymentButton').on('click', function () {
+            alert("Thanks for your shopping :)");
+        });
+        $('#CancelButton').on('click', function () {
+            var x;
+            if (confirm("Are You sure that you want to finish transaction?!") == true) {
+                x = window.open("index.html");
 
-        }
+            }
 
-    });
+        });
+     });
 
-
-    var leftBoxItemCost=
-        "<p cost='{{total}}'>Total: {{total}} kr</p>";
-
-
-});
-/*What we need today
- <span>{{pub_price}} kr</span> //credit
-
- var leftBoxItem //for total & credit
- var leftBoxItemRed //if credit is minus
-
- */
 
