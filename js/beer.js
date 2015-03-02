20cafdce27b7104e69f4a6a3a6bb432d63d47731
peralbin <peralbin@ponty.se>
2015-03-02 14:35:29 +0100

    Undo/redo working kind of.

--- js/beer.js
+++ js/beer.js
@@ -20,7 +20,7 @@ $(document).ready(function() {
     var rightListItem = 
         "<li data-id='{{id}}'>{{name}} <span id='{{id}}'>({{amount}})</span><p><br>" +
         "{{price}} kr</p><button class='button' data-id='{{id}}'" +
-        "id=remove price='{{price}}'>X</button></li>";
+        "id=remove price='{{price}}' name='{{name}}'>X</button></li>";
 
     var costItem = 
         "<p>---</p><p cost='{{total}}'>Total: {{total}} kr</p>";
@@ -50,6 +50,151 @@ $(document).ready(function() {
                 }
             }
         },
+    };
+    var order = {
+        orderList: {},
+        //Methods
+        load: function() {
+            orderObj = this;
+            $rightList.empty();
+            $.each(this.orderList, function(key, value) {
+                $rightList.append(Mustache.render(rightListItem, value));
+            }); 
+        },
+        addOrder: function(id, name, thisPrice) {
+            if (beerList.stockCount[id] > 0) {
+                beerList.stockCount[id] -= 1;
+                if (name.length > 18) {
+                    name = name.substring(0, 15).concat("...");
+                }
+                if (this.orderList[id] != undefined) {
+                    this.orderList[id]['amount'] += 1;
+                } else {
+                    this.orderList[id] = {'id': id, 'name': name, 'price': thisPrice, 'amount': 1};
+                }
+                order.load();
+                priceObj.total = priceObj.total + parseInt(thisPrice);
+                priceObj.addCost();
+            } else {
+                alert('No more of this beer in stock!'); 
+            }
+        },
+        removeOrder: function(id, thisPrice) {
+            var thisOrder = this.orderList[id];
+            if (thisOrder != undefined){
+                beerList.stockCount[id] += 1;
+                if (thisOrder['amount'] == 1) {
+                    delete this.orderList[id];
+                    $("ul li[data-id=" + id + "]").remove();
+                } else {
+                    thisOrder['amount'] -= 1;
+                }
+                order.load();
+                priceObj.total = priceObj.total-parseInt(thisPrice);
+                priceObj.addCost(priceObj);
+            };
+        },
+    };
+    var button = {
+        //Methods
+        click: function(theCommand) {
+            theCommand.execute();
+        },
+        clickUndo: function(theCommand) {
+            theCommand.undo();
+        },
+        clickRedo: function(theCommand) {
+            theCommand.redo();
+        },
+    };
+    function addToOrder(theReceiver, theId, theName, thePrice) {
+        this.id = theId;
+        this.name = theName;
+        this.price = thePrice;
+        this.receiver = theReceiver;
+        //Methods
+        this.execute = function() {
+            this.receiver.addOrder(this.id, this.name, this.price);
+        };
+        this.undo = function() {
+            this.receiver.removeOrder(this.id, this.price);
+        };
+        this.redo = function() {
+            this.receiver.addOrder(this.id, this.name, this.price);
+        };
+    };
+    function removeFromOrder(theReceiver, theId, theName, thePrice) {
+        this.id = theId;
+        this.name = theName;
+        this.price = thePrice;
+        this.receiver = theReceiver;
+        //Methods
+        this.execute = function() {
+            this.receiver.removeOrder(this.id, this.price);
+        };
+        this.undo = function() {
+            this.receiver.addOrder(this.id, this.name, this.price);
+        };
+        this.redo = function() {
+            this.receiver.removeOrder(this.id, this.price);
+        };
+    };
+    var priceObj = {
+        total: 0,
+        //Methods
+        addCost: function() {
+            var priceObject = this;
+            $cost.html(Mustache.render(costItem, priceObject));
+        }
+    };
+    var undoRedoStack = {
+        stack: [],
+        stackPointer: 0,
+        //Methods
+        lastCommand: null,
+        isUnder: function() {
+            if (this.stackPointer < 0) {
+                return true;
+            }
+            return false;
+        },
+        isOver: function() {
+            if (this.stackPointer > this.stack.length-1) {
+                return true;
+            }
+            return false
+        },
+        push: function(element) {
+            if (this.stack.length > 4) {
+                this.stack.splice(0, 1);
+            }
+            if (this.stack.length > this.stackPointer+1) { 
+                this.stack.splice(this.stackPointer+1, this.stack.length);
+            }
+            this.stack.push(element);
+            this.stackPointer = this.stack.length-1;
+            this.lastCommand = "push";
+        },
+        undo: function() {
+            if (this.lastCommand == "redo") {
+                this.stackPointer--;
+            } 
+            var result = this.stack[this.stackPointer];
+            this.stackPointer--;
+            this.lastCommand = "undo";
+            return result;
+        },
+        redo: function() {
+            if (this.lastCommand == "undo") {
+                this.stackPointer++;
+            } 
+            var result = this.stack[this.stackPointer];
+            this.stackPointer++;
+            this.lastCommand = "redo";
+            return result;
+        },
+    };
+    var jQueryBindings = {
         getAll: function() {
             $.ajax({
                 type: 'GET',
@@ -74,155 +219,66 @@ $(document).ready(function() {
                     }
                 });
             });
-        }
-    };
-    var order = {
-        orderList: {},
-        //Methods
-        load: function() {
-            orderObj = this;
-            $rightList.empty();
-            $.each(this.orderList, function(key, value) {
-                $rightList.append(Mustache.render(rightListItem, value));
-            }); 
-        },
-        addOrder: function(id, name, thisPrice, undoRedo) {
-            if (beerList.stockCount[id] > 0) {
-                beerList.stockCount[id] -= 1;
-                if (name.length > 18) {
-                    name = name.substring(0, 15).concat("...");
-                }
-                if (this.orderList[id] != undefined) {
-                    this.orderList[id]['amount'] += 1;
-                } else {
-                    this.orderList[id] = {'id': id, 'name': name, 'price': thisPrice, 'amount': 1};
-                }
-                order.load();
-                price.total = price.total + parseInt(thisPrice);
-                price.addCost();
-                if (undoRedoStack.stack.length > 9) {
-                    undoRedoStack.stack.splice(0, 1); 
-                }
-                if (!undoRedo) {
-                    undoRedoStack.push({"type": "removeOrder", "data": [id, thisPrice]});
-                }
-                console.log(undoRedoStack.stack);
-                console.log(undoRedoStack.currentIndex);
-            } else {
-                alert('No more of this beer in stock!'); 
-            }
-        },
-        removeOrder: function(id, thisPrice, undoRedo) {
-            var thisOrder = this.orderList[id];
-            if (thisOrder != undefined){
-                beerList.stockCount[id] += 1;
-                if (thisOrder['amount'] == 1) {
-                    delete this.orderList[id];
-                    $("ul li[data-id=" + id + "]").remove();
-                } else {
-                    thisOrder['amount'] -= 1;
-                    $('#' + thisOrder['id'] + '').text("(" + thisOrder['amount'] + ")");
-                }
-                price.total = price.total-parseInt(thisPrice);
-                price.addCost(price);
-                if (undoRedoStack.stack.length > 9) {
-                    undoRedoStack.stack.splice(0, 1); 
-                }
-                if (!undoRedo) {
-                    undoRedoStack.push({"type": "addOrder", "data": [id, thisOrder['name'], thisPrice]});
-                }
-                console.log(undoRedoStack.stack);
-                console.log(undoRedoStack.currentIndex);
-            };
         },
         addBind: function() {
             $leftList.delegate('#add', 'click', function() {
                 var id = $(this).attr('data-id');
                 var name = $(this).attr('name');
-                var thisPrice = $(this).attr('price');
-                order.addOrder(id, name, thisPrice, false);
+                var price = $(this).attr('price');
+                console.log(id);
+                console.log(name);
+                console.log(price);
+                var command = new addToOrder(order, id, name, price);
+                undoRedoStack.push(command);
+                button.click(command);
+                console.log(undoRedoStack);
             });
         },
         removeBind: function() {
             $rightList.delegate('#remove', 'click', function(){
                 var id = $(this).attr('data-id');
-                var thisPrice = $(this).attr('price');
-                order.removeOrder(id, thisPrice, false);
+                var name = $(this).attr('name');
+                var price = $(this).attr('price');
+                console.log(id);
+                console.log(name);
+                console.log(price);
+                var command = new removeFromOrder(order, id, name, price);
+                undoRedoStack.push(command);
+                button.click(command);
+            });
+        },
+        undoBind: function() {
+            $rightSide.delegate('#undoButton', 'click', function(){
+                if (!undoRedoStack.isUnder()) {
+                    var command = undoRedoStack.undo();
+                    button.clickUndo(command);
+                }
+            });
+        },
+        redoBind: function() {
+            $rightSide.delegate('#redoButton', 'click', function(){
+                if (!undoRedoStack.isOver()) {
+                    var command = undoRedoStack.redo();
+                    button.clickRedo(command);
+                }
             });
         },
         payBind: function() {
             $('#payButton').on('click', function() {
                 localStorage.setItem("order", JSON.stringify(order.orderList));
-                localStorage.setItem("total", price.total.toString());
+                localStorage.setItem("total", priceObj.total.toString());
             });
         },
-        undoBind: function() {
-            $('#undoButton').on('click', function() {
-                var action = undoRedoStack.undo();
-                if (action["type"] == "addOrder") {
-                    var data = action["data"];
-                    order.addOrder(data[0], data[1], data[2], true);
-                } else if(action["type"] == "removeOrder") {
-                    var data = action["data"];
-                    order.removeOrder(data[0], data[1], true);
-                }
-            });
-        },
-        redoBind: function() {
-            $('#redoButton').on('click', function() {
-                var action = undoRedoStack.redo();
-                if (action["type"] == "addOrder") {
-                    var data = action["data"];
-                    order.addOrder(data[0], data[1], data[2], true);
-                } else if(action["type"] == "removeOrder") {
-                    var data = action["data"];
-                    order.removeOrder(data[0], data[1], true);
-                }
-            });
-        },
-    };
-    var price = {
-        total: 0,
-        //Methods
-        addCost: function() {
-            var priceObject = this;
-            $cost.html(Mustache.render(costItem, priceObject));
-        }
-    };
-    var undoRedoStack = {
-        stack: [],
-        currentIndex: 0,
-        //Methods
-        push: function(element) {
-             this.stack.push(element); 
-             this.currentIndex = this.stack.length-1;
-        },
-        undo: function() {
-            var result = this.stack[this.currentIndex]; 
-            this.currentIndex -= 1;
-            if (this.currentIndex < 0) {
-                this.currentIndex = 0;
-            }
-            return result;
-        },
-        redo: function() {
-            var result = this.stack[this.currentIndex]; 
-            this.currentIndex += 1;
-            if (this.currentIndex >= this.stack.length) {
-                this.currentIndex = this.stack.length-1;
-            }
-            return result;
-        }
     };
 
     //AJAX REQUESTS AND JQUERY EVENTS
-    beerList.getAll();
-    beerList.searchBind();
-    order.addBind();
-    order.removeBind();
-    order.payBind();
-    order.undoBind();
-    order.redoBind();
+    jQueryBindings.getAll();
+    jQueryBindings.searchBind();
+    jQueryBindings.addBind();
+    jQueryBindings.removeBind();
+    jQueryBindings.payBind();
+    jQueryBindings.undoBind();
+    jQueryBindings.redoBind();
 
 
 
