d97973b9eb2926c63d90e9ebe0bf039eba29e6d2
thevanthony <thev.anthony@gmail.com>
2015-03-14 12:39:54 +0100

    Update mainObjects.js

--- js/mainObjects.js
+++ js/mainObjects.js
@@ -105,7 +105,7 @@ var jQueryBindings = {
     getAll: function() {
         $.ajax({
             type: 'GET',
-            url: 'http://pub.jamaica-inn.net/fpdb/api.php?username=jorass&password=jorass&action=inventory_get',
+            url: 'http://pub.jamaica-inn.net/fpdb/api.php?username=' + localStorage.getItem("user") + '&password='+ localStorage.getItem("password") + '&action=inventory_get',
             success: function(object) {
                 data = object['payload'];
                 $.each(data, function(i, beer) {
